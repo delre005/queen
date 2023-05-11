88d850e8a457ef50219c8fb3180a95a54a527b8a
delre005 <113313791+delre005@users.noreply.github.com>
2023-05-11 14:05:39 -0500

    Add files via upload

---
+++ js/queen.js
@@ -0,0 +1,340 @@
+$(document).ready(function() {
+
+    $(".slide-one").delay(1000).fadeIn("slow");
+    $(".slide-one").delay(5000).fadeOut("slow");
+
+    $(".slide-two").delay(7000).fadeIn("slow");
+    $(".slide-two").delay(6000).fadeOut("slow");
+
+    $(".slide-three").delay(16000).fadeIn("slow");
+    $(".slide-three").delay(3000).fadeOut("slow");
+
+    $(".slide-four").delay(21000).fadeIn("slow");
+    $(".slide-four").delay(3000).fadeOut("slow");
+
+    $(".slide-five").delay(27000).fadeIn("fast");
+
+  
+});
+
+
+
+$(document).ready(function() {
+
+    $(".unmute-notice").delay(500).fadeOut("fast");
+    $(".unmute-notice").delay(1000).fadeIn("fast");
+    $(".unmute-notice").delay(500).fadeOut("fast");
+    $(".unmute-notice").delay(2000).fadeIn("fast");
+    $(".unmute-notice").delay(500).fadeOut("fast");
+    $(".unmute-notice").delay(3000).fadeIn("fast");
+    $(".unmute-notice").delay(500).fadeOut("fast");
+    $(".unmute-notice").delay(4000).fadeIn("fast");
+    $(".unmute-notice").delay(500).fadeOut("fast");
+    $(".unmute-notice").delay(5000).fadeIn("fast");
+    $(".unmute-notice").delay(500).fadeOut("fast");
+
+   
+
+});
+
+// Bohemian Rhapsody
+
+$(document).ready(function() {
+
+    $(".line-one").delay(24000).fadeIn("fast").animate({fontSize: "180px"});
+    $(".line-one").delay(1000).fadeOut("fast");
+
+    $(".line-two").delay(26500).fadeIn("fast")
+    $(".killed").delay(26500).addClass("killed");
+    $(".line-two").delay(2000).fadeOut("fast");
+
+    $(".line-three").delay(29500).fadeIn("fast");
+    $(".gun").delay(29500).addClass("gun");
+    $(".dead").delay(34000).addClass("dead");
+    $(".line-three").delay(6000).fadeOut("fast");
+
+    $(".line-four").delay(36000).fadeIn("fast").animate({fontSize: "180px"});
+    $(".line-four").delay(1000).fadeOut("fast");
+
+    $(".line-five").delay(38000).fadeIn("fast").animate({fontSize: "100px"});
+    $(".line-five").delay(2000).fadeOut("fast");
+
+    $(".line-six").delay(42000).fadeIn("fast");
+    $(".line-six").delay(5700).fadeOut("fast");
+
+    $(".line-seven").delay(48000).fadeIn("fast")
+    $(".oooh").delay(48000).addClass("oooh");
+    $(".line-seven").delay(4000).fadeOut("fast");
+
+    $(".line-eight").delay(54000).fadeIn("fast");
+    $(".cry").delay(54000).addClass("cry");
+    $(".line-eight").delay(3000).fadeOut("fast");
+
+    $(".line-nine").delay(58000).fadeIn("fast");
+    $(".line-nine").delay(3000).fadeOut("fast");
+
+    $(".line-ten").delay(62000).fadeIn("fast");
+    $(".carry").delay(62000).addClass("carry");
+    $(".line-ten").delay(68000).fadeOut("fast");
+
+});
+
+// Radio Ga Ga
+
+$(document).ready(function() {
+
+    $(".radio-line-one").delay(1800).fadeIn("fast");
+    $(".radio-line-one").delay(3200).fadeOut("fast");
+
+    $(".radio-line-two").delay(5500).fadeIn("fast")
+    $(".radio-line-two").delay(4500).fadeOut("fast");
+
+    $(".radio-line-three").delay(10500).fadeIn("fast");
+    $(".just-one").delay(10500).addClass("just-one");
+    $(".just-two").delay(10500).addClass("just-two");
+    $(".radio-line-three").delay(3200).fadeOut("fast");
+
+    $(".radio-line-four").delay(14200).fadeIn("fast");
+    $(".just-three").delay(14200).addClass("just-three");
+    $(".radio-line-four").delay(3500).fadeOut("fast");
+
+    $(".radio-line-five").delay(18000).fadeIn("fast");
+    $(".time").delay(18000).addClass("time");
+    $(".power").delay(18000).addClass("power");
+    $(".radio-line-five").delay(4000).fadeOut("fast");
+
+    $(".radio-line-six").delay(22500).fadeIn("fast");
+    $(".radio-line-six").delay(3500).fadeOut("fast");
+
+    $(".radio-line-seven").delay(26200).fadeIn("fast");
+    $(".radio-lyric").delay(26200).addClass("radio-lyric");
+    $(".radio-line-seven").delay(2000).fadeOut("fast");
+
+    $(".radio-line-eight").delay(30000).fadeIn("fast");
+    $(".radio-pulse").delay(30000).addClass("radio-pulse");
+    $(".radio-line-eight").delay(3800).fadeOut("fast");
+
+    $(".radio-line-nine").delay(34000).fadeIn("fast");
+    $(".radio-pulse-single").delay(34000).addClass("radio-pulse-single");
+    $(".radio-line-nine").delay(1800).fadeOut("fast");
+
+    $(".radio-line-ten").delay(36000).fadeIn("fast");
+    $(".radio-pulse-single").delay(36000).addClass("radio-pulse-single");
+    $(".radio-line-ten").delay(1800).fadeOut("fast");
+
+    $(".radio-line-eleven").delay(38500).fadeIn("fast");
+    $(".radio-pulse").delay(38500).addClass("radio-pulse");
+    $(".radio-line-eleven").delay(3800).fadeOut("fast");
+
+    $(".radio-line-twelve").delay(42500).fadeIn("fast");
+    $(".radio-pulse-single").delay(42500).addClass("radio-pulse-single");
+    $(".radio-line-twelve").delay(2000).fadeOut("fast");
+
+    $(".radio-line-thirteen").delay(45000).fadeIn("fast");
+    $(".radio-line-thirteen").delay(3000).fadeOut("fast");
+
+    $(".radio-line-fourteen").delay(49000).fadeIn("fast");
+    $(".radio-line-fourteen").delay(7500).fadeOut("fast");
+});
+
+// Hammer to Fall
+
+$(document).ready(function() {
+
+    $(".hammer-line-one").delay(16500).fadeIn("fast");
+    $(".hammer-line-one").delay(3000).fadeOut("fast");
+
+    $(".hammer-line-two").delay(20500).fadeIn("fast")
+    $(".hammer-line-two").delay(2600).fadeOut("fast");
+
+    $(".hammer-line-three").delay(24000).fadeIn("fast");
+    $(".hammer-line-three").delay(2500).fadeOut("fast");
+
+    $(".hammer-line-four").delay(27500).fadeIn("fast");
+    $(".hammer-line-four").delay(2500).fadeOut("fast");
+
+    $(".hammer-line-five").delay(30500).fadeIn("fast");
+    $(".hammer-line-five").delay(3200).fadeOut("fast");
+
+    $(".hammer-line-six").delay(34000).fadeIn("fast");
+    $(".hammer-line-six").delay(3000).fadeOut("fast");
+
+    $(".hammer-line-seven").delay(37500).fadeIn("fast");
+    $(".hammer-line-seven").delay(2800).fadeOut("fast");
+
+    $(".hammer-line-eight").delay(41000).fadeIn("fast");
+    $(".hammer-line-eight").delay(3000).fadeOut("fast");
+
+   
+
+});
+
+// Crazy Little Thing Called Love
+
+$(document).ready(function() {
+
+    $(".crazy-line-one").delay(5500).fadeIn("fast");
+    $(".love").delay(5500).addClass("love");
+    $(".crazy-line-one").delay(3000).fadeOut("fast");
+
+    $(".crazy-line-two").delay(9000).fadeIn("fast")
+    $(".crazy-line-two").delay(2500).fadeOut("fast");
+
+    $(".crazy-line-three").delay(12000).fadeIn("fast");
+    $(".love").delay(12000).addClass("love");
+    $(".crazy-line-three").delay(2000).fadeOut("fast");
+
+    $(".crazy-line-four").delay(15000).fadeIn("fast");
+    $(".crazy-line-four").delay(2500).fadeOut("fast");
+
+    $(".crazy-line-five").delay(18200).fadeIn("fast");
+    $(".crazy-line-five").delay(1500).fadeOut("fast");
+
+    $(".crazy-line-six").delay(20000).fadeIn("fast");
+    $(".love").delay(20000).addClass("love");
+    $(".crazy-line-six").delay(2500).fadeOut("fast");
+
+    $(".crazy-line-seven").delay(24000).fadeIn("fast");
+    $(".love").delay(24000).addClass("love");
+    $(".crazy-line-seven").delay(2600).fadeOut("fast");
+
+    $(".crazy-line-eight").delay(27000).fadeIn("fast");
+    $(".crazy-line-eight").delay(3500).fadeOut("fast");
+
+    $(".crazy-line-nine").delay(31000).fadeIn("fast");
+    $(".crazy-line-nine").delay(2000).fadeOut("fast");
+
+    $(".crazy-line-ten").delay(34000).fadeIn("fast");
+    $(".crazy-line-ten").delay(1000).addClass("shakes");
+    $(".crazy-line-ten").delay(1500).fadeOut("fast");
+
+    $(".crazy-line-eleven").delay(37000).fadeIn("fast");
+    $(".crazy-line-eleven").delay(1000).fadeOut("fast");
+
+    $(".crazy-line-twelve").delay(39000).fadeIn("fast");
+    $(".love").delay(39000).addClass("love");
+    $(".crazy-line-twelve").delay(2000).fadeOut("fast");
+
+    $(".crazy-line-thirteen").delay(42000).fadeIn("fast");
+    $(".crazy-line-thirteen").delay(2500).fadeOut("fast");
+
+    $(".crazy-line-fourteen").delay(46800).fadeIn("fast");
+    $(".crazy-line-fourteen").delay(1700).fadeOut("fast");
+
+    $(".crazy-line-fifteen").delay(49000).fadeIn("fast");
+    $(".crazy-line-fifteen").delay(2000).fadeOut("fast");
+
+    $(".crazy-line-sixteen").delay(52500).fadeIn("fast");
+    $(".crazy-line-sixteen").delay(1300).fadeOut("fast");
+
+    $(".crazy-line-seventeen").delay(54100).fadeIn("fast");
+    $(".crazy-line-seventeen").delay(2000).fadeOut("fast");
+
+    $(".crazy-line-eighteen").delay(61000).fadeIn("fast");
+    $(".crazy-line-eighteen").delay(2500).fadeOut("fast");
+
+    $(".crazy-line-nineteen").delay(64000).fadeIn("fast");
+    $(".crazy-line-nineteen").delay(3500).fadeOut("fast");
+
+    $(".crazy-line-twenty").delay(68000).fadeIn("fast");
+    $(".crazy-line-twenty").delay(2500).fadeOut("fast");
+
+    $(".crazy-line-twentyone").delay(72000).fadeIn("fast");
+    $(".crazy-line-twentyone").delay(2000).fadeOut("fast");
+
+    $(".crazy-line-twentytwo").delay(74500).fadeIn("fast");
+    $(".crazy-line-twentytwo").delay(1000).fadeOut("fast");
+
+    $(".crazy-line-twentythree").delay(76500).fadeIn("fast");
+    $(".love").delay(76500).addClass("love");
+    $(".crazy-line-twentythree").delay(2000).fadeOut("fast");
+
+});
+
+// We Will Rock You
+
+$(document).ready(function() {
+
+    $(".rock-line-one").delay(20000).fadeIn("fast");
+    $(".rock-line-one").delay(1500).fadeOut("fast");
+
+    $(".rock-line-two").delay(22000).fadeIn("fast")
+    $(".rock-line-two").delay(2000).fadeOut("fast");
+
+    $(".rock-line-three").delay(25000).fadeIn("fast");
+    $(".rock-line-three").delay(2000).fadeOut("fast");
+
+    $(".rock-line-four").delay(27500).fadeIn("fast");
+    $(".rock-line-four").delay(2500).fadeOut("fast");
+
+    $(".rock-line-five").delay(30500).fadeIn("fast");
+    $(".rock-pulse").delay(30500).addClass("rock-pulse");
+    $(".rock-line-five").delay(2500).fadeOut("fast");
+
+    $(".rock-line-six").delay(35000).fadeIn("fast");
+    $(".rock-pulse").delay(35000).addClass("rock-pulse");
+    $(".rock-line-six").delay(3500).fadeOut("fast");
+
+    $(".rock-line-seven").delay(40000).fadeIn("fast");
+    $(".rock-pulse").delay(40000).addClass("rock-pulse");
+    $(".rock-line-seven").delay(3500).fadeOut("fast");
+
+    $(".rock-line-eight").delay(45000).fadeIn("fast");
+    $(".rock-pulse").delay(45000).addClass("rock-pulse");
+    $(".rock-line-eight").delay(3500).fadeOut("fast");
+
+});
+
+// We Are the Champions
+
+$(document).ready(function() {
+
+    $(".champions-line-one").delay(7500).fadeIn("fast");
+    $(".champions-line-one").delay(2500).fadeOut("fast");
+
+    $(".champions-line-two").delay(10500).fadeIn("fast")
+    $(".champions-line-two").delay(2000).fadeOut("fast");
+
+    $(".champions-line-three").delay(14000).fadeIn("fast");
+    $(".champions-line-three").delay(2000).fadeOut("fast");
+
+    $(".champions-line-four").delay(17000).fadeIn("fast");
+    $(".champions-line-four").delay(2500).fadeOut("fast");
+
+    $(".champions-line-five").delay(21000).fadeIn("fast");
+    $(".champions-line-five").delay(2000).fadeOut("fast");
+
+    $(".champions-line-six").delay(25000).fadeIn("fast");
+    $(".champions-line-six").delay(2000).fadeOut("fast");
+
+    $(".champions-line-seven").delay(28000).fadeIn("fast");
+    $(".champions-line-seven").delay(1700).fadeOut("fast");
+
+    $(".champions-line-eight").delay(30000).fadeIn("fast");
+    $(".champions-line-eight").delay(1500).fadeOut("fast");
+
+    $(".champions-line-nine").delay(32000).fadeIn("fast");
+    $(".champions-line-nine").delay(2000).fadeOut("fast");
+
+    // $(".champions-line-ten").delay(0).fadeIn("fast");
+    // $(".champions-line-ten").delay(0).fadeOut("fast");
+
+    $(".champions-line-eleven").delay(38000).fadeIn("fast");
+    $(".champions-line-eleven").delay(5000).fadeOut("fast");
+
+    $(".champions-line-twelve").delay(45500).fadeIn("fast");
+    $(".champions-line-twelve").delay(4500).fadeOut("fast");
+
+    $(".champions-line-thirteen").delay(52500).fadeIn("fast");
+    $(".champions-line-thirteen").delay(3000).fadeOut("fast");
+
+    $(".champions-line-fourteen").delay(56000).fadeIn("fast");
+    $(".champions-line-fourteen").delay(2800).fadeOut("fast");
+
+    $(".champions-line-fifteen").delay(59000).fadeIn("fast");
+    $(".champions-line-fifteen").delay(3000).fadeOut("fast");
+
+    $(".champions-line-sixteen").delay(62500).fadeIn("fast");
+    $(".champions-line-sixteen").delay(10000).fadeOut("fast");
+
+});
